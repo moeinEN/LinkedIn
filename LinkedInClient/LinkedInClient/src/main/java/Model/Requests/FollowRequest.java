f165a13545d6bcca2464630a02071da8f13a5e3a
moeinEN <moenayatti@gmail.com>
2024-07-04 16:15:57 +0330

    aaaa

---
+++ LinkedInClient/LinkedInClient/src/main/java/Model/Requests/FollowRequest.java
@@ -0,0 +1,12 @@
+package Model.Requests;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FollowRequest {
+    private int identificationCode;
+}
